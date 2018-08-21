b71b490f132a766965a7900e6491ded6dbd91052
Mayuresh Kshirsagar <yks@u.cash>
2018-08-21 20:02:00 +0530

    'adding1'

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -9,7 +10,7 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
-
+//ObjectId("5b7bc455c334ba0d18df4005")
 app.post('/todos', (req, res) => {
 
 	//console.log(req.body);
@@ -31,6 +32,27 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+app.get('/todos/:id', (req, res) => {
+//res.send(req.params);
+
+var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');

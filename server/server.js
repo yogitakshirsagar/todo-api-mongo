517e9b212c6a48ccec971576c28ce2d0e07f3195
Mayuresh Kshirsagar <yks@u.cash>
2018-08-21 19:04:00 +0530

    'adding'

--- server/server.js
+++ server/server.js
@@ -11,12 +11,11 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-	//console.log(req.body);
 
+	//console.log(req.body);
   var todo = new Todo({
     text: req.body.text
-	
- });
+  });
 
   todo.save().then((doc) => {
     res.send(doc);
@@ -25,10 +24,17 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
-
 
 //mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
